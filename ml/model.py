49088342fd82edf07f4d3e9f062520572d694c00
akshayDhotre <a2dhotre@gmail.com>
2023-11-26 10:42:29 +0530

    Updates in model trainng and added code in main

--- ml/model.py
+++ ml/model.py
@@ -1,5 +1,14 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+import logging
+import multiprocessing
+
+logging.basicConfig(filename=os.path.join(file_dir, './logs/ml_model.log'),
+                    level=logging.INFO,
+                    filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 
 # Optional: implement hyperparameter tuning.
@@ -19,8 +28,26 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    rfc_model = RandomForestClassifier()
+    parameters = {
+        'n_estimators': [10, 15, 20],
+        'max_depth': [5, 10],
+        'min_samples_split': [20, 50, 100],
+        'learning_rate': [1.0],
+    }
+
+    n_jobs = multiprocessing.cpu_count() - 1
+    logging.info(f"Finding optimum values of hyperparameters on {n_jobs} cores.")
+
+    rfc_model = GridSearchCV(RandomForestClassifier(random_state=0),
+                       param_grid=parameters,
+                       cv=3,
+                       n_jobs=njobs,
+                       verbose=2,
+                       )
+
     rfc_model.fit(X_train, y_train)
+    logging.info(f"BEST PARAMS: {rfc_model.best_params_}")
+
     return rfc_model
 
 
@@ -62,3 +89,68 @@ def inference(model, X):
     """
     predictions = model.predict(X)
     return predictions
+
+def compute_confusion_matrix(y, preds, labels=None):
+    """
+    Compute confusion matrix on predictions.
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    ------
+    cm : confusion matrix for the provided prediction set
+    """
+    cm = confusion_matrix(y, preds)
+    return cm
+
+def compute_slice_metrics(df, feature, y, preds):
+    """
+    Compute the performance on slices for a given categorical feature
+    a slice corresponds to one value option of the categorical feature analyzed
+    
+    Inputs
+    ------
+    df: 
+        test dataframe pre-processed with features as column used for slices
+    feature:
+        feature on which to perform the slices
+    y : np.array
+        corresponding known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized
+
+    Returns
+    ------
+    Dataframe with
+        n_samples: integer - number of data samples in the slice
+        precision : float
+        recall : float
+        fbeta : float
+    row corresponding to each of the unique values taken by the feature (slice)
+    """    
+    slice_options = df[feature].unique().tolist()
+    perf_df = pd.DataFrame(index=slice_options, 
+                            columns=['feature','n_samples','precision', 'recall', 'fbeta'])
+    for option in slice_options:
+        slice_mask = df[feature]==option
+
+        slice_y = y[slice_mask]
+        slice_preds = preds[slice_mask]
+        precision, recall, fbeta = compute_model_metrics(slice_y, slice_preds)
+        
+        perf_df.at[option, 'feature'] = feature
+        perf_df.at[option, 'n_samples'] = len(slice_y)
+        perf_df.at[option, 'precision'] = precision
+        perf_df.at[option, 'recall'] = recall
+        perf_df.at[option, 'fbeta'] = fbeta
+
+    # reorder columns in performance dataframe
+    perf_df.reset_index(names='feature value', inplace=True)
+    colList = list(perf_df.columns)
+    colList[0], colList[1] =  colList[1], colList[0]
+    perf_df = perf_df[colList]
+
+    return perf_df
--- ml/train_model.py
+++ ml/train_model.py
@@ -1,22 +1,50 @@
-# Script to train machine learning model.
+"""
+Script to train machine learning model.
+"""
 
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 import os
-import pandas as pd
-from ml.data import process_data
-from ml.model import train_model, inference, compute_model_metrics
 import pickle
-from slices import compute_slice_metrics
+import logging
+import pandas as pd
+from data import process_data
+from model import train_model, inference, compute_model_metrics
+from model import compute_confusion_matrix, compute_slice_metrics
+
 
 # Add code to load in the data.
 file_dir = os.path.dirname(__file__)
-data = pd.read_csv(os.path.join(file_dir, './data/census.csv'))
+datapath = pd.read_csv(os.path.join(file_dir, './data/census.csv'))
+
+# model save path artifacts
+modelpath = './model'
+filename = ['rfc_model.pkl', 'encoder.pkl', 'labelizer.pkl']
+
+# Initialize logging
+logging.basicConfig(filename=os.path.join(file_dir, './logs/ml_model.log'),
+                    level=logging.INFO,
+                    filemode='a',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+
+# Function to remove files given the path
+def remove_if_exists(filename):
+    """
+    Delete a file if it exists.
+    input:
+        filename: str - path to the file to be removed
+    output:
+        None
+    """
+    if os.path.exists(filename):
+        os.remove(filename)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=10, stratify=data['salary'])
 
+# Define categorical features
 cat_features = [
     "workclass",
     "education",
@@ -27,37 +55,55 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
+# Set train flag to False to use the encoding from the train set
 X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label='salary', training=False, encoder=encoder, lb=lb)
 
-# Train and save a model.
-rfc_model = train_model(X_train, y_train)
+# Check if saved model exist and load them
+if os.path.isfile(os.path.join(file_dir, modelpath,filename[0])):
+        model = pickle.load(open(os.path.join(file_dir, modelpath,filename[0]), 'rb'))
+        encoder = pickle.load(open(os.path.join(file_dir, modelpath,filename[1]), 'rb'))
+        lb = pickle.load(open(os.path.join(file_dir, modelpath,filename[2]), 'rb'))
 
-model_path = os.path.join(file_dir, './model/rfc_model.pkl')
-pickle.dump(rfc_model, open(model_path, 'wb'))
+# Train and save a model if does not exist
+else:
+    rfc_model = train_model(X_train, y_train)
 
-encoder_path = os.path.join(file_dir, './model/encoder.pkl')
-pickle.dump(encoder, open(encoder_path, 'wb'))
+    model_path = os.path.join(file_dir, modelpath, filename[0])
+    pickle.dump(rfc_model, open(model_path, 'wb'))
 
-lb_path = os.path.join(file_dir, './model/lb.pkl')
-pickle.dump(lb, open(lb_path, 'wb'))
+    encoder_path = os.path.join(file_dir, modelpath, filename[1])
+    pickle.dump(encoder, open(encoder_path, 'wb'))
+
+    lb_path = os.path.join(file_dir, modelpath, filename[2])
+    pickle.dump(lb, open(lb_path, 'wb'))
 
 # Evaluate the model
 
-y_pred = inference(rfc_model, X_test)
-precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
-print(f"Precision = {precision}, Recall = {recall}, FBeta = {fbeta}")
+preds = inference(rfc_model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-# Check slices
-compute_slice_metrics(
-        model=rfc_model,
-        encoder=encoder,
-        lb=lb,
-        cleaned_df=data,
-        categorical_features=cat_features,
-        slice_features='education'
-    )
+logging.info(f"Classification target labels: {list(lb.classes_)}")
+
+logging.info(
+    f"precision:{precision:.3f}, recall:{recall:.3f}, fbeta:{fbeta:.3f}")
+
+cm = compute_confusion_matrix(y_test, preds, labels=list(lb.classes_))
+
+logging.info(f"Confusion matrix:\n{cm}")
+
+# Compute performance on slices for categorical features
+slice_savepath = "./slice_output.txt"
+remove_if_exists(slice_savepath)
+
+# iterate through the categorical features and save results to log and txt file
+for feature in cat_features:
+    performance_df = compute_slice_metrics(test, feature, y_test, preds)
+    performance_df.to_csv(slice_savepath,  mode='a', index=False)
+    logging.info(f"Performance on slice {feature}")
+    logging.info(performance_df)
